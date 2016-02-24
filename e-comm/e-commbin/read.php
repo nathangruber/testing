ec5cb50de8afec2d42857b939757d16f5b533076
nathangruber <grubernp@gmail.com>
2016-02-23 18:17:55 -0600

    fixed html in index.php

--- e-comm/e-commbin/read.php
+++ e-comm/e-commbin/read.php
@@ -22,7 +22,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
  
 <body>
