01964a99d0eeb92a7a48400541ca37dacea066da
nathangruber <grubernp@gmail.com>
2016-02-28 18:00:46 -0600

    testing login

--- e-comm/e-commaddress/login.php
+++ e-comm/e-commaddress/login.php
@@ -56,3 +56,22 @@
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
+<?php
+session_start();
+$message="";
+if(count($_POST)>0) {
+$conn = mysql_connect("localhost","root","");
+mysql_select_db("database",$conn);
+$result = mysql_query("SELECT * FROM user WHERE user_name='" . $_POST["user_name"] . "' and password = '". $_POST["password"]."'");
+$row  = mysql_fetch_array($result);
+if(is_array($row)) {
+$_SESSION["user_id"] = $row[user_id];
+$_SESSION["user_name"] = $row[user_name];
+} else {
+$message = "Invalid Username or Password!";
+}
+}
+if(isset($_SESSION["user_id"])) {
+header("Location:user_dashboard.php");
+}
+?>
