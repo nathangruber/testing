3ed1c79b9d1c636cef7848cc361a5d75a9cb1f12
nathangruber <grubernp@gmail.com>
2016-02-29 18:11:08 -0600

    edited view

--- e-comm/e-commcustomer/read.php
+++ e-comm/e-commcustomer/read.php
@@ -10,7 +10,7 @@
     } else {
         $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT * FROM customers where id = ?";
+        $sql = "SELECT * FROM customer where id = ?";
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         $data = $q->fetch(PDO::FETCH_ASSOC);
