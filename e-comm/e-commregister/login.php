10ff06b28da97cfe3c5e0b05f57d311d262d050c
nathangruber <grubernp@gmail.com>
2016-03-01 20:03:59 -0600

    test

--- e-comm/e-commregister/login.php
+++ e-comm/e-commregister/login.php
@@ -18,9 +18,9 @@ $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 // Selecting Database
-$db = mysql_select_db("register", $connection);
+$db = mysql_select_db("customer", $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from register where password='$password' AND username='$username'", $connection);
+$query = mysql_query("select * from customer where password='$password' AND username='$username'", $connection);
 $rows = mysql_num_rows($query);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session
