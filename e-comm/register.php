70cf147d5e354940dd2251af1b382ee486094d97
nathangruber <grubernp@gmail.com>
2016-03-02 17:56:34 -0600

    test

--- e-comm/register.php
+++ e-comm/register.php
@@ -79,7 +79,6 @@
 
 
 <!DOCTYPE html>
-<?php require_once('includes/navbar.php');?>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -88,96 +87,98 @@
 </head>
  
 <body>
-    
     <div class="container">
-      <div class="span10 offset1">
-        <div class="row">
-          <h3>Please Register.</h3>
-        </div>           
-        <form class="form-horizontal" action="register.php" method="post"> 
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Register</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="register.php" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($birth_dateError)?'error':'';?>">
+                        <label class="control-label">Date of Birth</label>
+                        <div class="controls">
+                            <input name="birth_date" type="text" placeholder="Date of Birth" value="<?php echo !empty($birth_date)?$birth_date:'';?>">
+                            <?php if (!empty($birth_dateError)): ?>
+                                <span class="help-inline"><?php echo $birth_dateError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($genderError)?'error':'';?>">
+                        <label class="control-label">Gender</label>
+                        <div class="controls">
+                            <input name="gender" type="text"  placeholder="Gender" value="<?php echo !empty($gender)?$gender:'';?>">
+                            <?php if (!empty($genderError)): ?>
+                                <span class="help-inline"><?php echo $genderError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      
+                      <div class="control-group <?php echo !empty($phone_numberError)?'error':'';?>">
+                        <label class="control-label">Phone Number</label>
+                        <div class="controls">
+                            <input name="phone_number" type="text"  placeholder="Phone Number" value="<?php echo !empty($phone_number)?$phone_number:'';?>">
+                            <?php if (!empty($phone_numberError)): ?>
+                                <span class="help-inline"><?php echo $phone_numberError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
 
-          <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
-            <label class="control-label">Name</label>
-            <div class="controls">
-              <input name="name" type="text" placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
-              <?php if (!empty($nameError)): ?>
-                <span class="help-inline"><?php echo $nameError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
+                      <div class="control-group <?php echo !empty($email_addressError)?'error':'';?>">
+                        <label class="control-label">Email Address</label>
+                        <div class="controls">
+                            <input name="email_address" type="text"  placeholder="Email Address" value="<?php echo !empty($email_address)?$email_address:'';?>">
+                            <?php if (!empty($email_addressError)): ?>
+                                <span class="help-inline"><?php echo $email_addressError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
 
-          <div class="control-group <?php echo !empty($birth_dateError)?'error':'';?>">
-            <label class="control-label">Birthdate</label>
-            <div class="controls">
-              <input name="birth_date" type="text" placeholder="Birthday" value="<?php echo !empty($birth_date)?$birth_date:'';?>">
-              <?php if (!empty($birth_dateError)): ?>
-                <span class="help-inline"><?php echo $birth_dateError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
+                      <div class="control-group <?php echo !empty($permissionsError)?'error':'';?>">
+                        <label class="control-label">Permissions</label>
+                        <div class="controls">
+                            <input name="permissions" type="text"  placeholder="Permissions" value="<?php echo !empty($permissions)?$permissions:'';?>">
+                            <?php if (!empty($permissionsError)): ?>
+                                <span class="help-inline"><?php echo $permissionsError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($usernameError)?'error':'';?>">
+                        <label class="control-label">Username</label>
+                        <div class="controls">
+                            <input name="username" type="text"  placeholder="Username" value="<?php echo !empty($username)?$username:'';?>">
+                            <?php if (!empty($usernameError)): ?>
+                                <span class="help-inline"><?php echo $usernameError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                       <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+                        <label class="control-label">Password</label>
+                        <div class="controls">
+                            <input name="password" type="text"  placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
+                            <?php if (!empty($passwordError)): ?>
+                                <span class="help-inline"><?php echo $passwordError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                     
 
-          <div class="control-group <?php echo !empty($genderError)?'error':'';?>">
-            <label class="control-label">Gender</label>
-            <div class="controls">
-              <input name="gender" type="text" placeholder="Gender" value="<?php echo !empty($gender)?$gender:'';?>">
-              <?php if (!empty($genderError)): ?>
-                <span class="help-inline"><?php echo $genderError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
-
-          <div class="control-group <?php echo !empty($phone_numberError)?'error':'';?>">
-            <label class="control-label">Phone Number</label>
-            <div class="controls">
-              <input name="phone_number" type="text" placeholder="Phone Number" value="<?php echo !empty($phone_number)?$phone_number:'';?>">
-              <?php if (!empty($phone_numberError)): ?>
-                <span class="help-inline"><?php echo $phone_numberError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
-
-          <div class="control-group <?php echo !empty($email_addressError)?'error':'';?>">
-            <label class="control-label">Email Address</label>
-            <div class="controls">
-              <input name="email_address" type="text" placeholder="Email Address" value="<?php echo !empty($email_address)?$email_address:'';?>">
-              <?php if (!empty($email_addressError)): ?>
-                <span class="help-inline"><?php echo $email_addressError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
-
-          <div class="control-group <?php echo !empty($usernameError)?'error':'';?>">
-            <label class="control-label">Username</label>
-            <div class="controls">
-              <input name="username" type="text" placeholder="Username" value="<?php echo !empty($username)?$username:'';?>">
-              <?php if (!empty($usernameError)): ?>
-                <span class="help-inline"><?php echo $usernameError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
-
-          <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
-            <label class="control-label">Password</label>
-            <div class="controls">
-              <input name="password" type="text" placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
-              <?php if (!empty($passwordError)): ?>
-                <span class="help-inline"><?php echo $passwordError;?></span>
-              <?php endif;?>
-            </div>
-          </div>
-                        
-          <div class="form-actions">
-            <button type="submit" class="btn btn-success">Register</button>
-            <!-- no longer need a button to go back as this is the page being updated   <a class="btn" href="index.php">Back</a>   -->
-          </div>
-        </form>
-      </div>
-    </div>
-
-    <?php require_once('includes/footer.php');?>
-
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-
- </body>
-</html>
+                             <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+  </html>
