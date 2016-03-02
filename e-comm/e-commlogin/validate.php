c689cb53b3915910829d36498f1dda8ce06584c3
nathangruber <grubernp@gmail.com>
2016-03-02 15:13:31 -0600

    testing

--- e-comm/e-commlogin/validate.php
+++ e-comm/e-commlogin/validate.php
@@ -1,36 +1,27 @@
 <?php
-// Define $username and $password
-require_once 'includes/database.php'
-$user =$_POST['username'];
-$pass =$_POST['password'];
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-  if ($valid) {
-           try {
+            require_once 'includes/database.php';
             $pdo = Database::connect();
+            $user = ($_POST["username"]);
+            $pass = ($_POST["password"]);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "SELECT * FROM customer WHERE username=? and password=?";
+            $sql = "SELECT * FROM customer WHERE username = ? AND password = ?";
             $q = $pdo->prepare($sql);
-            //save results for query
-            $q->execute(array($username,$password));
+            $q->execute(array($user,$pass));
             $data = $q->fetch(PDO::FETCH_ASSOC);
             $username = $data['username'];
-	        $id = $data['id'];
-	        $password = $data['password'];
-	        $permission = $data['permission'];
-            // if found
-            $_SESSION['username'] = $username
             $first = $data['first'];
             $last = $data['last'];
-            $_SESSION['id'] = $id
-            $_SESSION['password'] = $password
-            $_SESSION['permission'] = $permission
-        } catch (PDOException $e) {
-            //echo "msg: " . $e->getMessage();
-            //die);
-            Database::disconnect();
-            header("Location: index.php");
-        }
-  }
+            $id = $data['id'];
+            $password = $data['password'];
+            $permission = $data['permission'];
+            Database::disconnect(); 
+            session_start();
+            
+            $_SESSION["userid"] = $id;
+            $_SESSION["first"] = $first;
+            $_SESSION["last"] = $last;
+            $_SESSION["username"] = $username;
+            $_SESSION["permission"] = $permission;
   if ($user == $username && $pass == $password) {
                 echo "Welcome " . $_SESSION["first"] . " You are logged in.";
                 echo  " in auth" . $_SESSION["userid"];
